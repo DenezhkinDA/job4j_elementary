084a2d11eb30de8ef10beaae20eefba120177da5
ДиЕ <denezhkin.dda@gmail.com>
2022-01-14 20:07:49 +0300

    4.2. Заголовок в файле java [#159036 #250532]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+      System.out.println("Dima Denezhkin");
+      System.out.println("01.03.1986");
+    }
+}
