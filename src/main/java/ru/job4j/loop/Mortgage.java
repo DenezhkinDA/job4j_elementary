bddd6674844d11b604acbf29097cf5c1734fe901
ДиЕ <denezhkin.dda@gmail.com>
2022-02-06 00:59:58 +0300

    5.7. Ипотека [#156315 #260807]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double remainder = amount;
-        while (remainder > 0) {
-            remainder = remainder + remainder * percent / 100 - salary;
-            year += 1;
-            }
-        return year;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
+            year++;
         }
+        return year;
     }
+}
