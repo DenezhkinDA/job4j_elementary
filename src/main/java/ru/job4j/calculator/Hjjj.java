9c86e87342d6f43e13fc9d13ff19ef9943a71e90
ДиЕ <denezhkin.dda@gmail.com>
2022-02-05 20:22:30 +0300

    5.8. Именование классов и переменных [#4087]

---
+++ src/main/java/ru/job4j/calculator/Hjjj.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Hjjj {
+    public static void main(String[] args) {
+        int income = 0;
+        while (income < 100) {
+            System.out.println(income);
+            income += 10;
+        }
+    }
+}
