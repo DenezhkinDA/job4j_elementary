1b00295eaf8eb70964103df646e5174eb6469849
ДиЕ <denezhkin.dda@gmail.com>
2022-02-24 21:19:18 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -5,29 +5,29 @@ import org.junit.Test;
 
 public class PointTest {
     @Test
-    public void when00to02then2() {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        a.distance(b);
-        double expected = 2;
-        Assert.assertEquals(expected, a.distance(b), 0.01);
+    public void when002to024then2dot82() {
+        Point a = new Point(0, 0, 2);
+        Point b = new Point(0, 2, 4);
+        a.distance3d(b);
+        double expected = 2.82;
+        Assert.assertEquals(expected, a.distance3d(b), 0.01);
     }
 
     @Test
-    public void when13to57then5dot65() {
-        Point a = new Point(1, 3);
-        Point b = new Point(5, 7);
-        a.distance(b);
-        double expected = 5.65;
-        Assert.assertEquals(expected, a.distance(b), 0.01);
+    public void when132to575then6dot40() {
+        Point a = new Point(1, 3, 2);
+        Point b = new Point(5, 7, 5);
+        a.distance3d(b);
+        double expected = 6.40;
+        Assert.assertEquals(expected, a.distance3d(b), 0.01);
     }
 
     @Test
-    public void when58to79then2dot23() {
-        Point a = new Point(5, 8);
-        Point b = new Point(7, 9);
-        a.distance(b);
-        double expected = 2.23;
-        Assert.assertEquals(expected, a.distance(b), 0.01);
+    public void when584to791then3dot74() {
+        Point a = new Point(5, 8, 4);
+        Point b = new Point(7, 9, 1);
+        a.distance3d(b);
+        double expected = 3.74;
+        Assert.assertEquals(expected, a.distance3d(b), 0.01);
     }
 }
